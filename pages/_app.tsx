5da939206d338372c99337ca276f14c6f2cb5754
Data-Polar <support@datapolar.dev>
2025-09-13 13:12:41 +0200

    fix implementazione tema scuro di default + startup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { appWithTranslation } from "next-i18next";
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class">
+    <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
       <Component {...pageProps} />
     </ThemeProvider>
   );
